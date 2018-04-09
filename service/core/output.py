c6a29b66efb8138927022993fd85788ce202c542
boranx <boran.seref@gmail.com>
2018-04-09 13:50:48 +0300

    move endpoints to annotation

--- service/core/output.py
+++ service/core/output.py
@@ -3,42 +3,38 @@ from flask import jsonify
 from core import api, celery, app
 import tasks, json
 
-
-class TaskOutput(Resource):
-    def get(self, task_id):
-        """Show a Task Output
-        ---
-        tags:
-        - Tasks
-        parameters:
-            - in: path
-              name: task_id
-              type: string
-              required: true
-              description: The uuid of your task.
-        responses:
-            200:
-                description: Show a Task Output
-                schema:
-                    $ref: '#/command/output/<TaskId>'
-                examples:
-                    {
-                        "output": "foo\n",
-                        "hostname": "celery@vagrant",
-                        "returncode": 0,
-                        "description": "Task run successfully"
-                    }
-            """
-        task = tasks.cmd_runner.AsyncResult(task_id)
-        if task.state == 'PENDING':
-            result = "Task is waiting for execution or unknown"
-            resp = app.make_response((jsonify(status=result), 420))
-            return resp
-        result = task.info
-        jsonResult = json.dumps(result, ensure_ascii=False)
-        resp = app.make_response((jsonResult, 200))
-        resp.headers['content-type'] = 'text/plain'
+@app.route("/command/output/<string:task_id>", methods=['GET'])
+def output(task_id):
+    """Show a Task Output
+    ---
+    tags:
+    - Tasks
+    parameters:
+        - in: path
+          name: task_id
+          type: string
+          required: true
+          description: The uuid of your task.
+    responses:
+        200:
+            description: Show a Task Output
+            schema:
+                $ref: '#/command/output/<TaskId>'
+            examples:
+                {
+                    "output": "foo\n",
+                    "hostname": "celery@vagrant",
+                    "returncode": 0,
+                    "description": "Task run successfully"
+                }
+        """
+    task = tasks.cmd_runner.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        result = "Task is waiting for execution or unknown"
+        resp = app.make_response((jsonify(status=result), 420))
         return resp
-
-
-api.add_resource(TaskOutput, '/command/output/<string:task_id>')
+    result = task.info
+    # jsonResult = json.dumps(result, ensure_ascii=False)
+    # resp = app.make_response((jsonResult, 200))
+    # resp.headers['content-type'] = 'text/plain'
+    return jsonify(result), 200
--- service/core/status.py
+++ service/core/status.py
@@ -4,54 +4,51 @@ from core import api, celery, app
 import tasks
 
 
-class TaskStatus(Resource):
-    def get(self, task_id):
-        """Show a Task Status
-        This is using docstrings for specifications.
-        ---
-        tags:
-        - Tasks
-        parameters:
-            - in: path
-              name: task_id
-              type: string
-              required: true
-              description: The uuid of your task.
-        responses:
-            200:
-                description: Show a Task Status
-                schema:
-                    $ref: '#/command/status/<TaskId>'
-                examples:
-                    {
-                        "Status": "SUCCESS",
-                        "description": "Task run successfully"
-                    }
-            """
-        task = tasks.cmd_runner.AsyncResult(task_id)
-        if task.state == 'PENDING':
-            result = "Task is waiting for execution or unknown"
-            resp = app.make_response((jsonify(status=result), 420))
-            return resp
-        elif task.state == 'PROGRESS':
-            result_obj = {'Status': "PROGRESS",
-                          'description': "Task is currently running",
-                          'returncode': None}
-        else:
-            try:
-                return_code = task.info['returncode']
-                description = task.info['description']
-                if return_code is 0:
-                    result_obj = {'Status': "SUCCESS",
-                                  'description': description}
-                else:
-                    result_obj = {'Status': "TASK_FAILURE",
-                                  'description': description,
-                                  'returncode': return_code}
-            except:
-                result_obj = {'Status': "CELERY_FAILURE"}
+@app.route("/command/status/<string:task_id>", methods=['GET'])
+def status(task_id):
+    """Show a Task Status
+    This is using docstrings for specifications.
+    ---
+    tags:
+    - Tasks
+    parameters:
+        - in: path
+          name: task_id
+          type: string
+          required: true
+          description: The uuid of your task.
+    responses:
+        200:
+            description: Show a Task Status
+            schema:
+                $ref: '#/command/status/<TaskId>'
+            examples:
+                {
+                    "Status": "SUCCESS",
+                    "description": "Task run successfully"
+                }
+        """
+    task = tasks.cmd_runner.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        result = "Task is waiting for execution or unknown"
+        resp = app.make_response((jsonify(status=result), 420))
+        return resp
+    elif task.state == 'PROGRESS':
+        result_obj = {'Status': "PROGRESS",
+                        'description': "Task is currently running",
+                        'returncode': None}
+    else:
+        try:
+            return_code = task.info['returncode']
+            description = task.info['description']
+            if return_code is 0:
+                result_obj = {'Status': "SUCCESS",
+                                'description': description}
+            else:
+                result_obj = {'Status': "TASK_FAILURE",
+                                'description': description,
+                                'returncode': return_code}
+        except:
+            result_obj = {'Status': "CELERY_FAILURE"}
 
-        return result_obj
-
-
-api.add_resource(TaskStatus, '/command/status/<string:task_id>')
+    return jsonify(result_obj), 200
